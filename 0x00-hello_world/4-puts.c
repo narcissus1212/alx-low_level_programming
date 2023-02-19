15f312c4b834e7e56085d8defbfdea574a46f03f
narcissus1212 <zz570418@gmail.com>
2023-02-19 08:17:32 -0800

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *this function prints a tst
+ *main - this function prints a text
+ * return : 0 (success) 
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
