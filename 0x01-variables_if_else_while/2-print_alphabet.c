56066198d633c1947837f37e53360206ec94ec42
narcissus1212 <zz570418@gmail.com>
2023-02-19 15:13:54 -0800

    my commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,16 +6,15 @@
 *Return:0
 *
 */
-
 int main(void)
 {
-	char c = 'a';
-	
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-	putchar('\n');
-	return (0);
+char c = 'a';
+
+while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+putchar('\n');
+return (0);
 }
