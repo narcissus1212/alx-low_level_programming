2af84d41e12b182ac41c5a5504884621e21b5021
narcissus1212 <zz570418@gmail.com>
2023-02-19 15:08:39 -0800

    my commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main block
+*description: get a rasndom it with s
+*Return:0
+*
+*/
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
